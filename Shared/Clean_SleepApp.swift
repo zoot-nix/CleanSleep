b463e206f0474c79927119d79044309e1a7f3a26
Owais Shaikh <theowaiszoot@gmail.com>
2022-06-30 22:20:18 +0530

    Initial Commit

---
+++ Shared/Clean_SleepApp.swift
@@ -0,0 +1,17 @@
+//
+//  Clean_SleepApp.swift
+//  Shared
+//
+//  Created by Owais Shaikh on 30/06/22.
+//
+
+import SwiftUI
+
+@main
+struct Clean_SleepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
